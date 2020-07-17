ed8f12a56ec8ce4e60494cbd4f2f18273cae004c
Ernesto W. Ulloa Gomez <ernestoulloa@gmail.com>
2020-07-17 13:00:29 -0300

    Updates

---
+++ precache-manifest.a7350f3f14285d7aa5a984e2ca91faf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451937742e417bca347b30a7d62dff06",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "7002cf03b82dae21bf5d",
+    "url": "/react-weather-app/static/css/main.eb9fb46c.chunk.css"
+  },
+  {
+    "revision": "9a81a82a4cc74d02c9a8",
+    "url": "/react-weather-app/static/js/2.292562cf.chunk.js"
+  },
+  {
+    "revision": "6ff3be09b66a6bae4c4000c31aff339c",
+    "url": "/react-weather-app/static/js/2.292562cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7002cf03b82dae21bf5d",
+    "url": "/react-weather-app/static/js/main.9eaa8cdf.chunk.js"
+  },
+  {
+    "revision": "afb9d3b11ebd943e02df",
+    "url": "/react-weather-app/static/js/runtime-main.457f1287.js"
+  }
+]);
